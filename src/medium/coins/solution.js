d954cf4a1c7f8c5c72defd5a6836ef8037928e2d
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2022-11-22 17:53:22 +0000

    Changed coins to show more info in the result.

--- src/medium/coins/solution.js
+++ src/medium/coins/solution.js
@@ -1,16 +1,51 @@
-function findDenomination (number) {
-    let denominations = [1, 5, 10, 25, 100]
-    let n = denominations.length;
-    let result = [];
+let arrayOfdenoms = [];
+let number = 0;
 
-    for ( let i = n-1; i>=0; i--){
-
-        while (number>= denominations[i]){
-            number -= denominations[i];
-            result.push(denominations[i]);
-        }
-    }
-return result
+function initiate(number) {
+  number = number;
+  findDenomination(number);
+  return occurances(arrayOfdenoms);
 }
 
-console.log(findDenomination(34))
+function findDenomination(number) {
+  let denominations = [1, 5, 10, 25, 100];
+  let n = denominations.length;
+
+  for (let i = n - 1; i >= 0; i--) {
+    while (number >= denominations[i]) {
+      number -= denominations[i];
+      arrayOfdenoms.push(denominations[i]);
+    }
+  }
+
+  return arrayOfdenoms;
+}
+
+function occurances(arrayOfdenoms) {
+  const denoms = arrayOfdenoms;
+  // console.log("Value of denoms const: ", denoms);
+  let result;
+
+  let hundreds = 0;
+  let twentyfives = 0;
+  let tens = 0;
+  let fives = 0;
+  let ones = 0;
+  denoms.forEach((element) => {
+    if (element === 100) {
+      hundreds += 1;
+    } else if (element === 25) {
+      twentyfives += 1;
+    } else if (element === 10) {
+      tens += 1;
+    } else if (element === 5) {
+      fives += 1;
+    } else if (element === 1) {
+      ones += 1;
+    }
+  });
+  result = `There are: ${hundreds} x 100, ${twentyfives} x 25, ${tens} x 10, ${fives} x 5, ${ones} x 1.`;
+  return result;
+}
+
+console.log(initiate(2427));
